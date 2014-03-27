ecb745001332f2c026aaa145ac7c7d9394ef7ab3
Alvin Alexander <devdaily@gmail.com>
2014-03-26 18:27:34 -0600

    cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+Finance (Server Component)
+==========================
+
+This is the server-side component of a "Finance" application that I'm
+working on. It's written in Scala and the Play Framework. The front-end
+will be written in JavaScript, and all of the server side controller
+methods will send and receive JSON.
+
+
--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -2,7 +2,6 @@
 # Notes: 
 # - `Ups` and `Downs` comments are important to Play.
 
-
 # --- !Ups
 
 create table users (
@@ -43,24 +42,14 @@ create table transactions (
 ) engine = InnoDB;
 
 
-# create table stock_prices (
-#   id int auto_increment not null,
-#   stock_id int not null,
-#   date_time timestamp not null default now(),
-#   price decimal(15,2) not null default 0,
-#   primary key (id),
-#   foreign key (stock_id) references stocks (id) on delete cascade
-# );
-
-
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS = 0;
-drop table if exists stock_prices;
-drop table if exists users;
-drop table if exists stocks;
-drop table if exists transactions;
-SET FOREIGN_KEY_CHECKS = 1;
+#SET FOREIGN_KEY_CHECKS = 0;
+#drop table if exists stock_prices;
+#drop table if exists users;
+#drop table if exists stocks;
+#drop table if exists transactions;
+#SET FOREIGN_KEY_CHECKS = 1;
 
 
 
